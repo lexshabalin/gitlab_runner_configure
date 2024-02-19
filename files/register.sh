6620104abda01a4f814290ab3ca68495aa3e4f00
AlexShabalin <lexshabalin@gmail.com>
2024-02-19 20:45:25 +0700

    rename tasks

--- files/register.sh
+++ files/register.sh
@@ -3,4 +3,5 @@ sudo gitlab-runner register \
   --url "https://gitlab.com/" \
   --token "glrt--Ykr-R6swPtghmPqvibz" \
   --executor "docker" \
+  --docker-image alpine:latest \
   --description "docker-runner"
